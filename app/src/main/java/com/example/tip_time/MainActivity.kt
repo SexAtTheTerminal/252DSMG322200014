45bcc1164d122b7e4b6e112ec40ce39f003b7b5b
Gabriel Cuba <gabrielcuba2017@gmail.com>
2025-09-26 02:10:41 -0500

    Unit Tests Finalized

--- app/src/main/java/com/example/tip_time/MainActivity.kt
+++ app/src/main/java/com/example/tip_time/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.annotation.DrawableRes
 import androidx.compose.material3.Icon
 import androidx.annotation.StringRes
+import androidx.annotation.VisibleForTesting
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.layout.Column
@@ -164,7 +165,8 @@ fun RoundTheTipRow(
     }
 }
 
-private fun calculateTip(amount: Double, tipPercent: Double = 15.0, roundUp: Boolean): String {
+@VisibleForTesting
+internal fun calculateTip(amount: Double, tipPercent: Double = 15.0, roundUp: Boolean): String {
     var tip = tipPercent / 100 * amount
 
     if(roundUp){
